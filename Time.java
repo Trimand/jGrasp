8661fa04042ef233d79046d0745c83ed70ee5f2c
Michael\loler <Trimand91@gmail.com>
2017-08-28 11:39:49 +0200

    v.1

--- Time.java
+++ Time.java
@@ -1,11 +1,22 @@
 public class Time{
-public static void main(String [] args){
-
-int second = 25;
-String minute = "03";
-String hour = "01";
-
-System.out.println(hour + ":" + minute + ":" + second);
-
-}
+   public static void main(String [] args){
+   
+   
+      int hour = 10;
+      int minute = 50;
+      int second = 25;
+      float percentage; 
+      percentage = (39025/864);
+   
+      System.out.println(hour + ":" + minute + ":" + second);
+   
+   
+      System.out.println("Number of seconds since midnight");
+      System.out.println((60*hour*60) + (60*minute) + second);
+      System.out.println("Remaining seconds of the day");
+      System.out.println((hour+13)*60*60);
+      System.out.println("Shows current percentage of the day that have passed");
+      System.out.println(percentage + "%");
+   
+   }
 }
